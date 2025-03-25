c03cc030f0ed96817e5b94ec4b101f0bf3a0b8e4
Hugo Loiola <hugoloiola.1@gmail.com>
2025-03-25 14:06:30 -0300

    Começando as structs1.rs

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -1,5 +1,6 @@
 fn main() {
     // You can optionally experiment here.
+
 }
 
 #[cfg(test)]
@@ -10,8 +11,11 @@ mod tests {
     fn move_semantics4() {
         let mut x = Vec::new();
         let y = &mut x;
-        let z = &mut x;
+        // `y` used here.
         y.push(42);
+        // The mutable reference `y` is not used anymore,
+        // therefore a new reference can be created.
+        let z = &mut x;
         z.push(13);
         assert_eq!(x, [42, 13]);
     }
